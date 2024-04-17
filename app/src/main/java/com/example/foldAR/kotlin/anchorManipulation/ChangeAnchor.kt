39e5f7f9d46bec010049bcab70a5f84de32df240
Max Teetz <max-julien@hotmail.de>
2024-04-17 19:35:45 +0200

    chore: readability and comments

--- app/src/main/java/com/example/foldAR/kotlin/anchorManipulation/ChangeAnchor.kt
+++ app/src/main/java/com/example/foldAR/kotlin/anchorManipulation/ChangeAnchor.kt
@@ -2,43 +2,44 @@ package com.example.foldAR.kotlin.anchorManipulation
 
 
 import android.graphics.Bitmap
-import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 
-//To map it on the coordinate system and use a Bitmap of Size 500, 500
+/**To map it on the coordinate system and use a Bitmap of Size 500, 500*/
 class ChangeAnchor {
+
+    /**if a bitmap of different size is used change this value or assign it direct!!*/
+    companion object {
+        const val offset = 250f //always half of used bitmap. I found that 500 is an acceptable size
+    }
+
+    //its bitmap.size/scaleFactor/2 in meters at the views edges
     private var scaleFactor: Int = 500
-    private lateinit var anchor: FloatArray
+    private var anchor: FloatArray? = null
 
-    fun setScaleFactor(factor: Int) {
-        scaleFactor = factor
-    }
+    private var distanceX = 0f
+    private var distanceZ = 0f
+    private var distanceY = 0f
 
-    private var oldX = 0f
-    private var oldZ = 0f
-    private var oldY = 0f
+    //returns the new anchors specific value plus moved distance
+    val newX get() = anchor?.get(0)?.plus(calculateNewPosition(distanceX)) ?: 0f
+    val newY get() = anchor?.get(1)?.minus(calculateNewPosition(distanceY)) ?: 0f
+    val newZ get() = anchor?.get(2)?.plus(calculateNewPosition(distanceZ)) ?: 0f
 
-    val newX get() = anchor[0] + calculateNewPosition(oldX)
-    val newY get() = anchor[1] - calculateNewPosition(oldY)
-    val newZ get() = anchor[2] + calculateNewPosition(oldZ)
-
+    //to center object in the bitmap and don`t only use values within it
     private fun calculatePoints(value: Float): Float {
-        return (value - 250).coerceIn(-250f, 250f)
+        return (value - offset).coerceIn(-offset, offset)
     }
 
-    private fun calculateNewPosition(old: Float) = old / scaleFactor
+    private fun calculateNewPosition(distance: Float) = distance / scaleFactor
 
     private fun calculatePointsPlane(x: Float, y: Float) {
-        oldX = calculatePoints(x)
-        oldZ = calculatePoints(y)
-        Log.d("calcLog", "$oldX + $oldZ")
+        distanceX = calculatePoints(x)
+        distanceZ = calculatePoints(y)
+        distanceY = calculatePoints(y)
     }
 
-    private fun calculatePointsHeight(y: Float) {
-        oldY = calculatePoints(y)
-    }
-
+    //get touch value and normalize it to the bitmaps size
     fun getNewPosition(event: MotionEvent, view: View, bitmap: Bitmap, anchorPos: FloatArray) {
         anchor = anchorPos
         val scaleFactorX = bitmap.width.toFloat() / view.width
@@ -48,6 +49,5 @@ class ChangeAnchor {
         val touchY = event.y * scaleFactorY
 
         calculatePointsPlane(touchX, touchY)
-        calculatePointsHeight(touchY)
     }
 }
